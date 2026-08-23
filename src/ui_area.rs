dda7fe8ec6dd79973c23b555b8f5865979dd5167
phntmSoap <hamad7anver@gmail.com>
2026-08-23 18:08:33 +0530

    partially finished ui_area

--- src/ui_area.rs
+++ src/ui_area.rs
@@ -1,4 +1,4 @@
-use ratatui::layout::Rect;
+use ratatui::layout::{self, Constraint, Direction, Layout, Rect};
 
 pub struct UiArea {
     title : Rect,
@@ -8,10 +8,57 @@ pub struct UiArea {
     ciphertext : Rect,
     instructions : Rect,
     cipher_list : Rect,
+    cipher : Rect,
 }
 
 impl UiArea {
     pub fn new(area : Rect) -> Self{
-        todo!()
+        let layouts = Layout::default()
+            .margin(1)
+            .direction(Direction::Horizontal)
+            .constraints([
+                Constraint::Length(1), //Title
+                Constraint::Length(30), // cipher vis
+                Constraint::Length(2), //Gap
+                /* 
+                Constraint::Length(1), //plaintext
+                Constraint::Length(3), //cipher list
+                Constraint::Length(1), //cipher text
+                Constraint::Length(1), //Gap
+                Constraint::Length(7), // Instructions
+                */
+                Constraint::Length(13),
+            ]).split(area);
+            let middle = Layout::default()
+                .margin(1);
+            let footer_middles = Layout::default()
+                .margin(1)
+                .direction(Direction::Vertical)
+                .constraints([
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(60),
+                    Constraint::Percentage(40),
+                ]).split(layouts[4]);
+
+            let footer_areas = Layout::default()
+                .margin(1)
+                .direction(Direction::Horizontal)
+                .constraints([
+                    Constraint::Length(1), //plaintext
+                    Constraint::Length(3), //cipher list
+                    Constraint::Length(1), //cipher text
+                    Constraint::Length(1), //Gap
+                    Constraint::Length(7), // Instructions
+                ]).split(footer_middles[1]);
+        Self {
+            title : todo!(),
+            cipher_visualization: todo!(),
+            history: todo!(),
+            plaintext: todo!(),
+            ciphertext: todo!(),
+            instructions: todo!(),
+            cipher_list: todo!(),
+            cipher: todo!(),
+        }
     }
 }
---
+++ target/debug/deps/cipherstacker-85dff1911c528515.d
@@ -0,0 +1,10 @@
+/Users/hamadhanver/cipherstacker/target/debug/deps/cipherstacker-85dff1911c528515.d: src/main.rs src/app.rs src/ciphermod.rs src/events.rs src/ui.rs src/ui_area.rs
+
+/Users/hamadhanver/cipherstacker/target/debug/deps/cipherstacker-85dff1911c528515: src/main.rs src/app.rs src/ciphermod.rs src/events.rs src/ui.rs src/ui_area.rs
+
+src/main.rs:
+src/app.rs:
+src/ciphermod.rs:
+src/events.rs:
+src/ui.rs:
+src/ui_area.rs:
