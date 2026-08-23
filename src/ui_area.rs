a7360de58e6f08cab4ba9e7fd5b3ca6d3e34a0d8
phntmSoap <hamad7anver@gmail.com>
2026-08-23 22:23:56 +0530

    finished Implementation of uiarea

--- src/ui_area.rs
+++ src/ui_area.rs
@@ -9,6 +9,7 @@ pub struct UiArea {
     instructions : Rect,
     cipher_list : Rect,
     cipher : Rect,
+    ciphers_side_bar : Rect,
 }
 
 impl UiArea {
@@ -25,9 +26,11 @@ impl UiArea {
                 Constraint::Length(3), //cipher list
                 Constraint::Length(1), //cipher text
                 Constraint::Length(1), //Gap
+                Constraint::Length(1), //cipher
+                Constraint::Length(1), //Gap
                 Constraint::Length(7), // Instructions
                 */
-                Constraint::Length(13),
+                Constraint::Length(15),
             ]).split(area);
             let middle = Layout::default()
                 .margin(1)
@@ -54,17 +57,20 @@ impl UiArea {
                     Constraint::Length(3), //cipher list
                     Constraint::Length(1), //cipher text
                     Constraint::Length(1), //Gap
+                    Constraint::Length(1), //cipher
+                    Constraint::Length(1), //Gap
                     Constraint::Length(7), // Instructions
                 ]).split(footer_middles[1]);
         Self {
-            title : todo!(),
-            cipher_visualization: todo!(),
-            history: todo!(),
-            plaintext: todo!(),
-            ciphertext: todo!(),
-            instructions: todo!(),
-            cipher_list: todo!(),
-            cipher: todo!(),
+            title : layouts[0],
+            cipher_visualization: middle[1],
+            history: footer_middles[2],
+            plaintext: footer_areas[0],
+            ciphertext: footer_areas[2],
+            instructions: footer_areas[6],
+            cipher_list: footer_areas[1],
+            cipher: footer_areas[4],
+            ciphers_side_bar : middle[0],
         }
     }
 }
