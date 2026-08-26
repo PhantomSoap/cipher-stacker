24165c210ba3226a0ef6ee661df253bc99e7a102
phntmSoap <hamad7anver@gmail.com>
2026-08-26 20:49:29 +0530

    atbash view struct

--- src/ui/ciphers_ui/atbash_ui.rs
+++ src/ui/ciphers_ui/atbash_ui.rs
@@ -1,10 +1,28 @@
 use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
-    text::Text,
-    widgets::{Block, Paragraph, Widget},
+    Frame, buffer::Buffer, layout::Rect, text::Text, widgets::{Block, Paragraph, Widget},
 };
+pub struct AtbashView {}
+impl AtbashView {
+    pub fn draw(frame : &mut Frame, area : Rect) {
+        let atbasher = format!(
+        "Atbash Cipher
+        {}
+        | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z |
+        | ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓   ↓ |
+        | Z | Y | X | W | V | U | T | S | R | Q | P | O | N | M | L | K | J | I | H | G | F | E | D | C | B | A |
+        {}
+        ",
 
+        "_".repeat(105),
+        "‾".repeat(105),  
+    );
+    frame.render_widget(Paragraph::new(Text::from(atbasher))
+        .centered()
+        .block(Block::bordered()),area)
+
+    
+    }
+}
 pub fn render_atbash(area: Rect, buf: &mut Buffer) {
     let atbasher = format!(
         "Atbash Cipher
